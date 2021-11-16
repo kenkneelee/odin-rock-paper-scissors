ce866c3d763016afc9e25a67b7cfc767b8f27184
Kenny Lee <kenny.lee927@gmail.com>
2021-11-16 03:28:29 -0500

    create index.html, main.js; link js file; pseudocode layout

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/* randomly returns rock, paper, or scissors */
+
+/* play single round using 2 inputs:  playerselection, computerselection
+returns string in format "You lose! Paper beats Rock"
+case insensitive */
+
+/* new function using previous function inside it to run 5 round game, keep score, return winner at end */
+
+/*use console.log to display results of each round and end winner */
+
+/* use prompt() to get input
+ */
